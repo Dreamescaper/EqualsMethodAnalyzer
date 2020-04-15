b105cc4a1385081f799f1d40fb7a39952110a049
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2020-04-15 20:30:42 +0300

    Add project files.

---
+++ EqualsMethodAnalyzer/UseEqualityOperatorInsteadOfMethodAnalyzer.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace EqualsMethodAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class UseEqualityOperatorInsteadOfMethodAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "EMA0001";
+
+        private static readonly DiagnosticDescriptor DiagnosticDescriptor = new DiagnosticDescriptor(
+                DiagnosticId,
+                "Use == operator instead of Equals method",
+                "Use == operator instead of Equals method for built-in types",
+                "Style",
+                DiagnosticSeverity.Warning,
+                true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; }
+            = ImmutableArray.Create(DiagnosticDescriptor);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterOperationAction(AnalyzeOperation, OperationKind.Invocation);
+        }
+
+        private void AnalyzeOperation(OperationAnalysisContext context)
+        {
+            if (!(context.Operation is IInvocationOperation invocationOperation))
+                return;
+
+            if (invocationOperation.TargetMethod?.Name == nameof(object.Equals)
+                && invocationOperation.Arguments.Length == 1
+                && invocationOperation.Instance != null
+                && IsBuiltInType(invocationOperation.Instance.Type))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptor, invocationOperation.Syntax.GetLocation()));
+            }
+        }
+
+        private static bool IsBuiltInType(ITypeSymbol typeSymbol)
+        {
+            switch (typeSymbol.SpecialType)
+            {
+                case SpecialType.System_Boolean:
+                case SpecialType.System_Char:
+                case SpecialType.System_SByte:
+                case SpecialType.System_Byte:
+                case SpecialType.System_Int16:
+                case SpecialType.System_UInt16:
+                case SpecialType.System_Int32:
+                case SpecialType.System_UInt32:
+                case SpecialType.System_Int64:
+                case SpecialType.System_UInt64:
+                case SpecialType.System_Decimal:
+                case SpecialType.System_Single:
+                case SpecialType.System_Double:
+                case SpecialType.System_String:
+                case SpecialType.System_DateTime:
+                    return true;
+            }
+
+            if (typeSymbol.BaseType?.SpecialType == SpecialType.System_Enum)
+                return true;
+
+            if (typeSymbol is INamedTypeSymbol namedTypeSymbol
+                && namedTypeSymbol.IsGenericType
+                && namedTypeSymbol.TypeArguments.Length == 1
+                && namedTypeSymbol.ConstructedFrom.SpecialType == SpecialType.System_Nullable_T)
+            {
+                return IsBuiltInType(namedTypeSymbol.TypeArguments[0]);
+            }
+
+            return false;
+        }
+    }
+}
---
+++ EqualsMethodAnalyzer/UseEqualityOperatorInsteadOfMethodCodeFixProvider.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace EqualsMethodAnalyzer
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp), Shared]
+    public class UseEqualityOperatorInsteadOfMethodCodeFixProvider : CodeFixProvider
+    {
+        private const string Title = "Use == operator";
+
+        public UseEqualityOperatorInsteadOfMethodCodeFixProvider()
+        {
+        }
+
+        public override ImmutableArray<string> FixableDiagnosticIds { get; }
+            = ImmutableArray.Create(UseEqualityOperatorInsteadOfMethodAnalyzer.DiagnosticId);
+
+        public override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var diagnostic = context.Diagnostics[0];
+
+
+            if (!(root.FindNode(context.Span) is InvocationExpressionSyntax invocationExpressionSyntax))
+                return;
+
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: Title,
+                    createChangedDocument: _ => ReplaceWithOperator(context.Document, root, invocationExpressionSyntax),
+                    equivalenceKey: Title),
+                diagnostic);
+        }
+
+        private Task<Document> ReplaceWithOperator(Document document, SyntaxNode root, InvocationExpressionSyntax invocationExpressionSyntax)
+        {
+            ExpressionSyntax nodeToReplace = invocationExpressionSyntax;
+
+            var left = ((MemberAccessExpressionSyntax)invocationExpressionSyntax.Expression).Expression;
+            var right = invocationExpressionSyntax.ArgumentList.Arguments[0].Expression;
+
+            var parentUnaryExpression = invocationExpressionSyntax.Parent as PrefixUnaryExpressionSyntax;
+
+            var operatorKind = SyntaxKind.EqualsExpression;
+            if (parentUnaryExpression?.IsKind(SyntaxKind.LogicalNotExpression) == true)
+            {
+                operatorKind = SyntaxKind.NotEqualsExpression;
+                nodeToReplace = parentUnaryExpression;
+            }
+
+            ExpressionSyntax operatorExpression = SyntaxFactory.BinaryExpression(operatorKind, left, right);
+
+            if (nodeToReplace.Parent is PrefixUnaryExpressionSyntax)
+                operatorExpression = SyntaxFactory.ParenthesizedExpression(operatorExpression);
+
+            operatorExpression = operatorExpression
+                .WithLeadingTrivia(invocationExpressionSyntax.GetLeadingTrivia())
+                .WithTrailingTrivia(invocationExpressionSyntax.GetTrailingTrivia());
+
+            var newRoot = root.ReplaceNode(nodeToReplace, operatorExpression);
+
+            return Task.FromResult(document.WithSyntaxRoot(newRoot));
+        }
+    }
+}
